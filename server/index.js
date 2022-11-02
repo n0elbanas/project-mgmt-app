b81936db3051269e9a82ca9b7de19bd3dbaf3036
Noel Banas <noel.banas@gmail.com>
2022-11-02 11:36:01 +0800

    CORS fixing

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ const app = express()
 connectDB()
 
 app.use(cors())
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  })
 
 app.use(
 	'/graphql',
